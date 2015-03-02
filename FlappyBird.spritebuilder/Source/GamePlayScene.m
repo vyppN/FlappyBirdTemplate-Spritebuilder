94c34ec1fb98cf7ceedc246c9a65697c280d3c4f
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-02 17:08:35 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,18 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacles = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacles += delta;
+    if (timeSinceObstacles > 2.0f) {
+        [self addObstacle];
+        
+        timeSinceObstacles = 0.0f;
+    }
 }
 
 // put new methods here
