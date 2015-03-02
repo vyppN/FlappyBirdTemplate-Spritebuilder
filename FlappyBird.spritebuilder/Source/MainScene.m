e0ceeb4bfc25aa12aa7f148a3b9e1c6fd203a4c9
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-02 19:10:20 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -9,6 +9,19 @@
 #import "MainScene.h"
 #import "Obstacle.h"
 
+@interface CGPointObject : NSObject{
+    CGPoint _ratio;
+    CGPoint _offset;
+    CCNode *__unsafe_unretained _child;
+}
+@property (nonatomic,readwrite) CGPoint ratio;
+@property (nonatomic,readwrite) CGPoint offset;
+@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
++(id)pointWithCGPoint:(CGPoint)point offset:(CGPoint) offset;
+-(id)initWithCGPoint:(CGPoint)point offset:(CGPoint) offset;
+
+@end
+
 @implementation MainScene {
     CCNode *_ground1;
     CCNode *_ground2;
@@ -33,6 +46,12 @@
     CCLabelTTF *_nameLabel;
     
     int points;
+    
+    CGPoint _cloudParallaxRatio;
+    CGPoint _bushParallaxRatio;
+    
+    CCNode *_parallaxContainer;
+    CCParallaxNode *_parallaxBackground;
 }
 
 
@@ -42,6 +61,25 @@
     _grounds = @[_ground1, _ground2];
     _clouds = @[_cloud1,_cloud2];
     _bushes = @[_bush1,_bush2];
+    
+    _parallaxBackground = [CCParallaxNode node];
+    [_parallaxContainer addChild:_parallaxBackground];
+    
+    //bush ratio larger than cloud
+    _bushParallaxRatio = ccp(0.9, 1);
+    _cloudParallaxRatio = ccp(0.6, 1);
+    
+    for (CCNode *bush in _bushes) {
+        CGPoint offset = bush.position;
+        [self removeChild:bush];
+        [_parallaxBackground addChild:bush z:0 parallaxRatio:_bushParallaxRatio positionOffset:offset];
+    }
+    
+    for (CCNode *cloud in _clouds) {
+        CGPoint offset = cloud.position;
+        [self removeChild:cloud];
+        [_parallaxBackground addChild:cloud z:0 parallaxRatio:_cloudParallaxRatio positionOffset:offset];
+    }
     
     for (CCNode *ground in _grounds) {
         // set collision txpe
