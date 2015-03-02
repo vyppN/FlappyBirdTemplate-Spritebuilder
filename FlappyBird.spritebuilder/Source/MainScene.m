2fc10d79f8f38bf19b0560d8471bdaf20dc4d7c7
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-02 18:58:05 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -154,6 +154,26 @@
         }
     }
     
+    // loop the bush
+    for (CCNode *bush in _bushes) {
+        bush.position = ccp(bush.position.x - (character.physicsBody.velocity.x * delta),bush.position.y);
+        
+        // if the left corner is one complete width off the screen, move it to the right
+        if (bush.position.x <= (-1 * bush.contentSize.width)) {
+            bush.position = ccp(bush.position.x + 2 * bush.contentSize.width, bush.position.y);
+        }
+    }
+    
+    // loop the clouds
+    for (CCNode *cloud in _clouds) {
+        cloud.position = ccp(cloud.position.x - (character.physicsBody.velocity.x * delta),cloud.position.y);
+        
+        // if the left corner is one complete width off the screen, move it to the right
+        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
+        }
+    }
+    
     NSMutableArray *offScreenObstacles = nil;
     
     for (CCNode *obstacle in _obstacles) {
